0f6cfdb7c2c46b05539a1dd6026f246b8c553502
Zbigniew Skowron <zbychs@gmail.com>
2016-02-22 00:02:09 +0100

    Added compile_time_string, element_formatter_t and attribute_formatter_t.

---
+++ source/utility/compile_time_string.h
@@ -0,0 +1,72 @@
+/////////////////////////////////////////////////////////////////////////////
+/// ArbitraryFormatSerializer
+///    Library for serializing data in arbitrary formats.
+///
+/// compile_time_string.h
+///
+/// This file contains compile_time_string that formats object as element of specific name.
+/// Element can be unnamed. Use then assign_name formatter to assign a name to it.
+///
+/// Distributed under Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+/// (c) 2016 Zbigniew Skowron, zbychs@gmail.com
+///
+/////////////////////////////////////////////////////////////////////////////
+
+#ifndef ArbitraryFormatSerializer_compile_time_string_H
+#define ArbitraryFormatSerializer_compile_time_string_H
+
+#include <string>
+#include <type_traits>
+
+namespace arbitrary_format
+{
+
+template <const char... Cs>
+struct compile_time_string
+{
+    using empty = std::bool_constant<sizeof...(Cs) == 0>;
+};
+
+template <typename str>
+struct compile_time_string_print;
+
+template <char... Cs>
+struct compile_time_string_print< compile_time_string<Cs...> >
+{
+    constexpr static std::string str()
+    {
+        return {Cs...};
+    }
+};
+
+template <typename T, typename str>
+struct make_compile_time_string2;
+
+template <size_t... Is, typename str>
+struct make_compile_time_string2<std::index_sequence<Is...>, str>
+{
+    using type = compile_time_string<str{}.chars[Is]...>;
+};
+
+template <int size, typename str>
+struct make_compile_time_string1
+{
+    using type = typename make_compile_time_string2<std::make_index_sequence<size>, str>::type;
+};
+
+#define make_compile_time_string(str) \
+( []() { \
+    struct constexpr_string_type { const char * chars = str; }; \
+    return make_compile_time_string1<sizeof(str) - 1, constexpr_string_type>::type(); \
+} () )
+
+#define declare_compile_time_string(name, str) \
+    auto declare_element_name_ ## name = make_compile_time_string(str); \
+    using name = decltype(declare_element_name_ ## name)
+
+//template<typename T, int N> using raw_array = T[N];
+//#define constant_string(str) (raw_array<const char, sizeof(str)> { str })
+
+} // namespace arbitrary_format
+
+#endif // ArbitraryFormatSerializer_compile_time_string_H
--- thrash/XmlSerializer.cpp
+++ thrash/XmlSerializer.cpp
@@ -12,6 +12,8 @@
 #include "xml_formatters/content_check.h"
 #include "xml_formatters/document_formatter.h"
 #include "xml_formatters/declaration_formatter.h"
+#include "xml_formatters/element_formatter_t.h"
+#include "xml_formatters/attribute_formatter_t.h"
 
 #include "formatters/vector_formatter.h"
 #include "formatters/optional_formatter.h"
@@ -20,6 +22,9 @@
 #include "formatters/tuple_formatter.h"
 
 #include "xml_serializers/RapidXmlTree.h"
+#include "binary_serializers/ISerializer.h"
+
+#include "utility/metaprogramming.h"
 
 #include <cstdint>
 #include <iostream>
@@ -31,108 +36,118 @@
 using namespace arbitrary_format;
 using namespace xml;
 
+
 int main(int argc, char* argv[])
 {
-	RapidXmlDocument document("utf-8");
-	RapidXmlDocument allDocument("utf-8");
+    RapidXmlDocument document("utf-8");
+    RapidXmlDocument allDocument("utf-8");
 
-	auto vec = std::vector<int> { 1, 2, 3, 4, 5 };
-	auto nameToAge = std::map<std::string, int> { {"Ela", 5}, {"Ala", 6}, {"Ola", 7} };
-	auto nameToAgeVec = std::vector<std::pair<std::string, int>> { {"Ela", 5}, {"Ala", 6}, {"Ola", 7} };
-	boost::optional<std::string> optNone;
-	boost::optional<std::string> optStr("A value!");
+    auto vec = std::vector<int> { 1, 2, 3, 4, 5 };
+    auto nameToAge = std::map<std::string, int> { {"Ela", 5}, {"Ala", 6}, {"Ola", 7} };
+    auto nameToAgeVec = std::vector<std::pair<std::string, int>> { {"Ela", 5}, {"Ala", 6}, {"Ola", 7} };
+    boost::optional<std::string> optNone;
+    boost::optional<std::string> optStr("A value!");
 
-	auto numberFormatter = create_attribute_formatter("number");
-	auto sizeFormatter = attribute_counter("number");
-	auto vectorFormatter = create_vector_formatter(sizeFormatter, numberFormatter);
-	auto vectorElementFormatter = create_element_formatter("numbers", vectorFormatter);
+    auto numberFormatter = create_attribute_formatter("number");
+    auto sizeFormatter = attribute_counter("number");
+    auto vectorFormatter = create_vector_formatter(sizeFormatter, numberFormatter);
+    auto vectorElementFormatter = create_element_formatter("numbers", vectorFormatter);
 
-	auto pairFormatter = create_tuple_formatter(assign_name<>(), assign_text_content<>());
-	auto valueFormatter = create_attribute_formatter(boost::none, pairFormatter);
-	auto vectorFormatter2 = create_vector_formatter(attribute_counter(), valueFormatter);
-	auto vectorElementFormatter2 = create_element_formatter("nameToAgeVec", vectorFormatter2);
+    auto pairFormatter = create_tuple_formatter(assign_name<>(), assign_text_content<>());
+    auto valueFormatter = create_attribute_formatter(boost::none, pairFormatter);
+    auto vectorFormatter2 = create_vector_formatter(attribute_counter(), valueFormatter);
+    auto vectorElementFormatter2 = create_element_formatter("nameToAgeVec", vectorFormatter2);
 
-	auto kvFormatter = create_attribute_formatter(boost::none, create_tuple_formatter(assign_name<>(), assign_text_content<>()));
-	auto mapFormatter = create_map_kv_formatter(attribute_counter(), kvFormatter);
-	auto mapElementFormatter = create_element_formatter("nameToAge", mapFormatter);
+    auto kvFormatter = create_attribute_formatter(boost::none, create_tuple_formatter(assign_name<>(), assign_text_content<>()));
+    auto mapFormatter = create_map_kv_formatter(attribute_counter(), kvFormatter);
+    auto mapElementFormatter = create_element_formatter("nameToAge", mapFormatter);
 
-	auto k2Formatter = create_attribute_formatter("value");
-	auto kvFormatter2 = create_element_formatter("key", create_tuple_formatter(k2Formatter, assign_text_content<>()));
-	auto mapFormatter2 = create_map_kv_formatter(element_counter(), kvFormatter2);
-	auto mapElementFormatter2 = create_element_formatter("nameToAge", mapFormatter2);
+    auto k2Formatter = create_attribute_formatter("value");
+    auto kvFormatter2 = create_element_formatter("key", create_tuple_formatter(k2Formatter, assign_text_content<>()));
+    auto mapFormatter2 = create_map_kv_formatter(element_counter(), kvFormatter2);
+    auto mapElementFormatter2 = create_element_formatter("nameToAge", mapFormatter2);
 
-	auto k3Formatter = create_element_formatter("key");
-	auto v3Formatter = create_element_formatter("value");
-	auto mapFormatter3 = create_map_formatter(element_counter("key"), k3Formatter, v3Formatter);
-	auto mapElementFormatter3 = create_element_formatter("nameToAge", mapFormatter3);
+    auto k3Formatter = create_element_formatter("key");
+    auto v3Formatter = create_element_formatter("value");
+    auto mapFormatter3 = create_map_formatter(element_counter("key"), k3Formatter, v3Formatter);
+    auto mapElementFormatter3 = create_element_formatter("nameToAge", mapFormatter3);
 
-	auto optionalFormatter = create_element_formatter("optional", create_optional_formatter(content_exists(), assign_text_content<>()));
+    auto optionalFormatter = create_element_formatter("optional", create_optional_formatter(content_exists(), assign_text_content<>()));
 
-	//auto doc_format = create_element_formatter< text_formatter<lexical_stringizer> >("myxml");
-	//doc_format.save(document.getDocumentElement(), 6);
+    declare_compile_time_string(optional, "optional");
+    declare_compile_time_string(initialized, "initialized");
+    declare_compile_time_string(value, "value");
+    using opt_format = element_formatter_t< optional, optional_formatter< attribute_formatter_t<initialized>, element_formatter_t<value> > >;
 
-	declaration_formatter<>().save(document, "utf-8");
-	vectorElementFormatter.save(document.getDocumentElement(), vec);
-	vectorElementFormatter2.save(document.getDocumentElement(), nameToAgeVec);
-	mapElementFormatter.save(document.getDocumentElement(), nameToAge);
-	mapElementFormatter2.save(document.getDocumentElement(), nameToAge);
-	mapElementFormatter3.save(document.getDocumentElement(), nameToAge);
-	optionalFormatter.save(document.getDocumentElement(), optNone);
-	optionalFormatter.save(document.getDocumentElement(), optStr);
+    //auto doc_format = create_element_formatter< text_formatter<lexical_stringizer> >("myxml");
+    //doc_format.save(document.getDocumentElement(), 6);
 
-	auto allFormatter = create_element_formatter("all", create_tuple_formatter(vectorElementFormatter, vectorElementFormatter2, mapElementFormatter));
+    declaration_formatter<>().save(document, "utf-8");
+    vectorElementFormatter.save(document.getDocumentElement(), vec);
+    vectorElementFormatter2.save(document.getDocumentElement(), nameToAgeVec);
+    mapElementFormatter.save(document.getDocumentElement(), nameToAge);
+    mapElementFormatter2.save(document.getDocumentElement(), nameToAge);
+    mapElementFormatter3.save(document.getDocumentElement(), nameToAge);
+    optionalFormatter.save(document.getDocumentElement(), optNone);
+    optionalFormatter.save(document.getDocumentElement(), optStr);
+    boost::optional<std::string> a("ala");
+    binary::save<opt_format>(document.getDocumentElement(), a);
 
-	allFormatter.save(document.getDocumentElement(), std::tie(vec, nameToAgeVec, nameToAge));
+    auto allFormatter = create_element_formatter("all", create_tuple_formatter(vectorElementFormatter, vectorElementFormatter2, mapElementFormatter));
 
-	auto docFormatter = create_document_formatter("doc", allFormatter);
-	docFormatter.save(allDocument, std::tie(vec, nameToAgeVec, nameToAge));
+    allFormatter.save(document.getDocumentElement(), std::tie(vec, nameToAgeVec, nameToAge));
 
-	std::stringstream str;
-	str << document;
-	std::string documentText = str.str();
-	std::cout << documentText;
+    auto docFormatter = create_document_formatter("doc", allFormatter);
+    docFormatter.save(allDocument, std::tie(vec, nameToAgeVec, nameToAge));
 
-	std::stringstream allStr;
-	allStr << allDocument;
-	std::string allDocumentText = allStr.str();
-	std::cout << allDocumentText;
+    std::stringstream str;
+    str << document;
+    std::string documentText = str.str();
+    std::cout << documentText;
 
-	RapidXmlDocument document2(documentText, "utf-8");
-	std::cout << document2;
-	RapidXmlDocument allDocument2(allDocumentText, "utf-8");
-	std::cout << allDocument2;
+    std::stringstream allStr;
+    allStr << allDocument;
+    std::string allDocumentText = allStr.str();
+    std::cout << allDocumentText;
 
-	std::string encoding;
-	std::vector<int> loadedVec;
-	std::vector<std::pair<std::string, int>> loadedNameToAgeVec;
-	std::map<std::string, int> loadedNameToAge;
-	std::map<std::string, int> loadedNameToAge2;
-	std::map<std::string, int> loadedNameToAge3;
-	boost::optional<std::string> optNone2;
-	boost::optional<std::string> optStr2;
+    RapidXmlDocument document2(documentText, "utf-8");
+    std::cout << document2;
+    RapidXmlDocument allDocument2(allDocumentText, "utf-8");
+    std::cout << allDocument2;
 
-	declaration_formatter<>().load(document2, encoding);
-	vectorElementFormatter.load(document2.getDocumentElement(), loadedVec);
-	vectorElementFormatter2.load(document2.getDocumentElement(), loadedNameToAgeVec);
-	mapElementFormatter.load(document2.getDocumentElement(), loadedNameToAge);
-	mapElementFormatter2.load(document2.getDocumentElement(), loadedNameToAge2);
-	mapElementFormatter3.load(document2.getDocumentElement(), loadedNameToAge3);
-	optionalFormatter.load(document2.getDocumentElement(), optNone2);
-	optionalFormatter.load(document2.getDocumentElement(), optStr2);
+    std::string encoding;
+    std::vector<int> loadedVec;
+    std::vector<std::pair<std::string, int>> loadedNameToAgeVec;
+    std::map<std::string, int> loadedNameToAge;
+    std::map<std::string, int> loadedNameToAge2;
+    std::map<std::string, int> loadedNameToAge3;
+    boost::optional<std::string> optNone2;
+    boost::optional<std::string> optStr2;
 
-	std::vector<int> loadedVec2;
-	std::vector<std::pair<std::string, int>> loadedNameToAgeVec2;
-	std::map<std::string, int> loadedNameToAge4;
+    declaration_formatter<>().load(document2, encoding);
+    vectorElementFormatter.load(document2.getDocumentElement(), loadedVec);
+    vectorElementFormatter2.load(document2.getDocumentElement(), loadedNameToAgeVec);
+    mapElementFormatter.load(document2.getDocumentElement(), loadedNameToAge);
+    mapElementFormatter2.load(document2.getDocumentElement(), loadedNameToAge2);
+    mapElementFormatter3.load(document2.getDocumentElement(), loadedNameToAge3);
+    optionalFormatter.load(document2.getDocumentElement(), optNone2);
+    optionalFormatter.load(document2.getDocumentElement(), optStr2);
+    boost::optional<std::string> b;
+    binary::load<opt_format>(document2.getDocumentElement(), b);
 
-	allFormatter.load(document2.getDocumentElement(), std::tie(loadedVec2, loadedNameToAgeVec2, loadedNameToAge4));
+    std::vector<int> loadedVec2;
+    std::vector<std::pair<std::string, int>> loadedNameToAgeVec2;
+    std::map<std::string, int> loadedNameToAge4;
 
-	std::vector<int> loadedVec5;
-	std::vector<std::pair<std::string, int>> loadedNameToAgeVec5;
-	std::map<std::string, int> loadedNameToAge5;
+    allFormatter.load(document2.getDocumentElement(), std::tie(loadedVec2, loadedNameToAgeVec2, loadedNameToAge4));
 
-	docFormatter.load(allDocument2, std::tie(loadedVec5, loadedNameToAgeVec5, loadedNameToAge5));
+    std::vector<int> loadedVec5;
+    std::vector<std::pair<std::string, int>> loadedNameToAgeVec5;
+    std::map<std::string, int> loadedNameToAge5;
 
-	std::cout << "Eaten Xml: '"<< document2 << "'";
+    docFormatter.load(allDocument2, std::tie(loadedVec5, loadedNameToAgeVec5, loadedNameToAge5));
 
-	return 0;
+    std::cout << "Eaten Xml: '"<< document2 << "'";
+
+    return 0;
 }
